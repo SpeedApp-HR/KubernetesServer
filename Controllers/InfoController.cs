224c28519da85d5b6f683097564b020167760a29
Yevgen Kochetenko <y.kochetenko@hensel-recycling.com>
2023-02-07 09:13:29 +0100

    Add project files.

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace KubernetesServer.SharedExtensions
+{
+
+    public class ServerInfo  
+    {
+        public string Version { get; set; } = "1.0";
+        public bool IsLive { get; set; } = false;
+        public string Server { get; set; } = "localhost";
+        public int Status { get; set; } = 1;
+        public string Message { get; set; } = "Online";
+        public string DatabaseServer { get; set; } = "localhost";
+        public string DatabaseName { get; set; } = "DB";
+        public string DatabaseStatus { get; set; } = "connected";
+        public string ClientVersion { get; set; } = "0.0.2";
+        public DateTime? FirstOnlineTime { get; set; } 
+        public DateTime? LastOfflineTime { get; set; }
+        public Dictionary<string, string> Attributes { get; set; }
+
+    }
+
+
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class InfoController : Controller
+    {
+        [HttpGet("Info")]
+        public  ServerInfo GetServerInfo()
+        {
+            ServerInfo info = new ServerInfo();
+            info.Version = "1.0";
+            //info.Attributes = new Dictionary<string, string>();
+            //info.Attributes.Add("url", WebApiConfig.OfficeFileServerUrl);
+            //info.Attributes.Add("version", OFServerInfo.Version);
+            //info.Attributes.Add("server", OFServerInfo.Server);
+            //info.Attributes.Add("databaseserver", OFServerInfo.DatabaseServer);
+            //info.Attributes.Add("databasename", OFServerInfo.DatabaseName);
+
+            return info;
+        }
+
+    }
+}
---
+++ Controllers/WebAppUserController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace CubirnatesDemoServer.SharedControllers
+{
+    public partial class WebAppUser
+    {
+        public WebAppUser()
+        {
+        }
+
+        public WebAppUser(string loginName, string shortName, string roleList, string name)
+        {
+            LoginName = loginName;
+            ShortName = shortName;
+            RoleList = roleList;
+            Name = name;
+        }
+
+        public int WebAppUserId { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+
+        public string LoginName { get; set; }
+
+        public string Password { get; set; } = string.Empty;
+        public string RoleList { get; set; }
+        public string WebAppRoleList { get; set; } = string.Empty;
+
+        public DateTime? JwtExpiredOn { get; set; }
+        public DateTime? LastLogin { get; set; }
+
+        [NotMapped]
+        public string Token { get; set; }
+
+        public string ClientVersion { get; set; }
+        [NotMapped]
+        public int TimeZoneOffsetInMin { get; set; }
+        [NotMapped]
+        public string Culture { get; set; }
+
+        public bool IsGermanCulture() => Culture?.Substring(0, 2).ToLower() == "de";
+
+        //generic Id  
+        public int GetEntityId() => WebAppUserId;
+        //generic ToString()
+        public override string ToString() => $"WebAppUserId: {GetEntityId()}; Name: {Name}";
+        //generic EntityInfo()
+        public string EntityInfo()
+        {
+            return $"WebAppUserId: {GetEntityId()}; Loginname:{LoginName}; JwtExpiredOn:{JwtExpiredOn}";
+
+        }
+    }
+
+
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public partial class WebAppUserController : Controller
+    {
+
+
+        [HttpPost("LoginWithNameAndPassword"),]
+        [Microsoft.AspNetCore.Authorization.AllowAnonymous]
+        public async Task<IActionResult> LoginWithNameAndPasswordAsync([FromBody] WebAppUser userFromClient)
+        {
+            try
+            {
+                userFromClient.ClientVersion = "0.0.1";
+                userFromClient.Name = "Test Kubernate";
+                userFromClient.RoleList = "Admin";
+                userFromClient.ShortName = "KT";
+                userFromClient.WebAppUserId = 1;
+                userFromClient.Culture = "de-DE";
+                return new OkObjectResult(userFromClient);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
